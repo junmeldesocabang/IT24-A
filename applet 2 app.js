558526fe9c6631d5123e439752787387bc84f669
junmeldesocabang <cabangjunmeldeso@gmail.com>
2024-10-04 11:19:20 +0800

    addMarker

---
+++ applet 2 app.js
@@ -0,0 +1,18 @@
+class LeafletMap {
+
+    constructor(containerId, center, zoom) {
+        this.map = L.map(containerId).setView(center, zoom);
+        this.initTileLayer();
+    }
+
+initTileLayer(){ 
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+}
+    addMarker(lat, lng, message) {
+        const marker = L.marker([lat, lng]).addTo(this.map);
+        marker.bindPopup(message);
+    }
+}
