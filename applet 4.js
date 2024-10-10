d9b738d85c71ffaf33c9bfbbf8c2ff87bc050d74
junmeldesocabang <cabangjunmeldeso@gmail.com>
2024-10-10 22:52:34 +0300

    Added fetchData

--- applet 4.js
+++ applet 4.js
@@ -12,7 +12,14 @@ class StudentList {
         this.bindSearchEvent();
 
     }
-}
-   
-   
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            this.students = await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        } 
+     }
+     
+    }
    
