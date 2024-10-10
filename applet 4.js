4250aae404c94040434ed5354050123cd1a9d46f
junmeldesocabang <cabangjunmeldeso@gmail.com>
2024-10-10 22:51:38 +0300

    Added async init

--- applet 4.js
+++ applet 4.js
@@ -5,10 +5,14 @@ class StudentList {
         this.students = [];
         this.init();
     }
-    
-}
+    async init() {
+        await this.fetchData();
+        this.renderStudentList(this.students); 
+      
+        this.bindSearchEvent();
 
-   
+    }
+}
    
    
    
