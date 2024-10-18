628701789871a2331a543c5bfdf790ad667d9160
junmeldesocabang <tawinlaspona@gmail.com>
2024-10-18 10:46:13 +0800

    added humidity

--- applet 5.js
+++ applet 5.js
@@ -7,3 +7,5 @@ class WeatherApp{
  this.cityName = document.getElementById('cityName');
  this.temperature = document.getElementById('temperature');
  this.description = document.getElementById('description');
+ this.humidity = document.getElementById('humidity');
+ 
