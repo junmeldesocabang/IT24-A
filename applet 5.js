b18603d2a4e3ad611e942c95c780e7f051bf3252
junmeldesocabang <tawinlaspona@gmail.com>
2024-10-18 10:52:13 +0800

    added weather service extends

--- applet 5.js
+++ applet 5.js
@@ -24,3 +24,10 @@ displayWeather(data){
     this.weatherCard.style.display = 'block';
 }
 }
+class WeatherService extends WeatherApp {
+
+    constructor() {
+        super();
+        this.apiKey = '';
+    }
+}
