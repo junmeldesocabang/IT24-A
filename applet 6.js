e0382a69d2b3cdb19688c5d4c8aa5dcb43b93d7b
Wenielrana07 <rana.weniel@gmail.com>
2024-10-25 14:04:48 +0800

    Added edit task

--- applet 6.js
+++ applet 6.js
@@ -50,5 +50,10 @@ updateTask(taskText) {
 removeTask(event) {
     this.todoList.removeChild(event.target.closest('.todo-item'));
 }
-
+editTask(event) {
+    const taskItem = event.target.closest('.todo-item');
+    this.todoInput.value = taskItem.querySelector('.task-text').textContent;
+    this.editingIndex = Array.from(this.todoList.children).indexOf(taskItem);
+    this.addButton.textContent = 'Update';
+}
 }
